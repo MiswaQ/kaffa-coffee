3f13a2b57c6facbdc3df719868404fc2d2921fb7
MiswaQ <miswaq@gmail.com>
2023-11-22 10:20:19 +0000

    Added homeapp and templates

--- home/apps.py
+++ home/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class KaffaConfig(AppConfig):
+class HomeConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'kaffa'
+    name = 'home'
