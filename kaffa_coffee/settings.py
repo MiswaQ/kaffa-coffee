b554ff89f3dfd37d76353c3d7b07bb27ee5ea79c
MiswaQ <miswaq@gmail.com>
2023-11-28 16:14:56 +0000

    Changed env and settings secret key

--- kaffa_coffee/settings.py
+++ kaffa_coffee/settings.py
@@ -26,10 +26,10 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['8000-miswaq-kaffacoffee-87f2oq9o52t.ws-eu106.gitpod.io', 'kaffa-coffee-1c360e949e52.herokuapp.com']
 
