17801e92f8bfff89503abb6edf54b8b4ce6ac488
MiswaQ <miswaq@gmail.com>
2024-05-12 22:32:30 +0000

    Added flickering colors to freeshiping text in base.html and base.css

--- kaffa_coffee/settings.py
+++ kaffa_coffee/settings.py
@@ -83,6 +83,7 @@ TEMPLATES = [
                 'django.template.context_processors.request', #required by allauth
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'bag.contexts.bag_contents',
             ],
         },
     },
@@ -192,3 +193,6 @@ if 'USER_AWS' in os.environ:
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+FREE_DELIVERY_THRESHOLD = 50
+STANDARD_DELIVERY_PERCENTAGE = 10
