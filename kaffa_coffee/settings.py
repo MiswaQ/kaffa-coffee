1353228bf08a116f7e84bdc371d75bb2b4373dbf
MiswaQ <miswaq@gmail.com>
2024-05-15 13:25:17 +0000

    Added checkout html and views

--- kaffa_coffee/settings.py
+++ kaffa_coffee/settings.py
@@ -55,6 +55,7 @@ INSTALLED_APPS = [
     'checkout',
 
     # Other
+    'crispy_forms',
     'storages',
 ]
 
@@ -70,6 +71,8 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'kaffa_coffee.urls'
 
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
@@ -84,8 +87,13 @@ TEMPLATES = [
                 'django.template.context_processors.request', #required by allauth
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
                 'bag.contexts.bag_contents',
             ],
+            'builtins': [
+                'crispy_forms.templatetags.crispy_forms_tags',
+                'crispy_forms.templatetags.crispy_forms_field',
+            ]
         },
     },
 ]
