178c03e35ec4c9f3cd6dc1f9c41c7918c1c42826
MiswaQ <miswaq@gmail.com>
2024-05-17 03:17:50 +0000

    Debug=True

--- kaffa_coffee/settings.py
+++ kaffa_coffee/settings.py
@@ -29,7 +29,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEVELOPMENT' in os.environ
+DEBUG = True  # 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['localhost', 'kaffa-coffee-1c360e949e52.herokuapp.com']
 
