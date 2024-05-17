3d2d97b58a5b3437bd47865a2a75a2544ce127bd
MiswaQ <miswaq@gmail.com>
2024-05-17 02:53:33 +0000

    Change url part 3

--- kaffa_coffee/settings.py
+++ kaffa_coffee/settings.py
@@ -176,12 +176,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
 if 'USER_AWS' in os.environ:
     # Bucket Config
